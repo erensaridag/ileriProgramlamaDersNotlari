94e067a13f8834db8074cdfd1ace1b4bdde13dba
Eren SARIDAG <erensaridag1@gmail.com>
2025-10-19 16:44:36 +0300

    feat:soru5

--- Sorular/1_HAFTA/soru5.py
+++ Sorular/1_HAFTA/soru5.py
@@ -1,7 +1,9 @@
 #Örnek 4: Ortalamaya göre öğrencinin sınıf geçme durumunu (geçti – kaldı) gösteren
 
-average = 65
-if average>60:
+quiz1 = int(input("Enter the first quiz grade : "))
+quiz2 = int(input("Enter the second quiz grade : "))
+averages = (quiz1+quiz2)/2
+if averages>60:
     print("Passed")
 else :
     print("Failed")
